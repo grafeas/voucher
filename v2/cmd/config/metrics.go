f4a52b36e4c1039964abad0093082c111adf385d
Peter Wagner <peter.wagner@shopify.com>
2021-08-27 08:18:41 -0400

    naming: StatsdClient

--- v2/cmd/config/metrics.go
+++ v2/cmd/config/metrics.go
@@ -3,30 +3,22 @@ package config
 import (
 	"log"
 
-	"github.com/DataDog/datadog-api-client-go/api/v1/datadog"
-	"github.com/DataDog/datadog-go/statsd"
 	"github.com/grafeas/voucher/v2/metrics"
 	"github.com/spf13/viper"
 )
 
 func MetricsClient() (metrics.Client, error) {
 	tags := viper.GetStringSlice("statsd.tags")
-	var client metrics.StatsdClient
-	if statsdAddr := viper.GetString("statsd.addr"); statsdAddr != "" {
-		var err error
-		client, err = statsd.New(statsdAddr, statsd.WithTags(tags))
-		if err != nil {
-			return nil, err
-		}
-	} else if ddApiKey := viper.GetString("stats.datadog.apikey"); ddApiKey != "" {
-		dd := datadog.NewAPIClient(datadog.NewConfiguration())
-		ddAppKey := viper.GetString("stats.datadog.appkey")
-		client = metrics.NewDatadogStatsd(dd, ddApiKey, ddAppKey, tags)
-	} else {
-		log.Printf("No metrics client configured")
-		return &metrics.NoopClient{}, nil
-	}
 
-	sampleRate := viper.GetFloat64("statsd.sample_rate")
-	return metrics.NewStatsdMetricsClient(client, sampleRate)
+	if statsdAddr := viper.GetString("statsd.addr"); statsdAddr != "" {
+		sampleRate := viper.GetFloat64("statsd.sample_rate")
+		return metrics.NewStatsdClient(statsdAddr, sampleRate, tags)
+	} else if ddApiKey := viper.GetString("statsd.datadog_apikey"); ddApiKey != "" {
+		// FIXME: bring back datadog
+		// dd := datadog.NewAPIClient(datadog.NewConfiguration())
+		// ddAppKey := viper.GetString("stats.datadog.appkey")
+		// client = metrics.NewDatadogStatsd(dd, ddApiKey, ddAppKey, tags)
+	}
+	log.Printf("No metrics client configured")
+	return &metrics.NoopClient{}, nil
 }
--- v2/metrics/statsd.go
+++ v2/metrics/statsd.go
@@ -6,71 +6,78 @@ import (
 	"github.com/DataDog/datadog-go/statsd"
 )
 
-type StatsdMetricsClient struct {
-	client       StatsdClient
-	samplingRate float64
-}
-
-func NewStatsdMetricsClient(client StatsdClient, samplingRate float64) (*StatsdMetricsClient, error) {
-	return &StatsdMetricsClient{
-		client:       client,
-		samplingRate: samplingRate,
-	}, nil
-}
-
-type StatsdClient interface {
+// statsdClient is the functionality of github.com/DataDog/datadog-go/statsd used by this implementation
+type statsdClient interface {
 	Incr(string, []string, float64) error
 	Timing(string, time.Duration, []string, float64) error
 	Event(*statsd.Event) error
 }
 
-var _ StatsdClient = (*statsd.Client)(nil)
+var _ statsdClient = (*statsd.Client)(nil)
 
-func (d *StatsdMetricsClient) CheckRunStart(check string) {
+// StatsdClient emits metrics via statsd protocol.
+type StatsdClient struct {
+	client       statsdClient
+	samplingRate float64
+}
+
+// NewStatsdClient creates a client to emit metrics to a statsd server, probably dogstatsd.
+func NewStatsdClient(addr string, samplingRate float64, tags []string) (*StatsdClient, error) {
+	client, err := statsd.New(addr, statsd.WithTags(tags))
+	if err != nil {
+		return nil, err
+	}
+	return &StatsdClient{
+		client:       client,
+		samplingRate: samplingRate,
+	}, nil
+}
+
+func (d *StatsdClient) CheckRunStart(check string) {
 	_ = d.client.Incr("voucher.check.run.start", []string{"check:" + check}, d.samplingRate)
 }
 
-func (d *StatsdMetricsClient) CheckRunLatency(check string, dur time.Duration) {
+func (d *StatsdClient) CheckRunLatency(check string, dur time.Duration) {
 	_ = d.client.Timing("voucher.check.run.latency", dur, []string{"check:" + check}, d.samplingRate)
 }
 
-func (d *StatsdMetricsClient) CheckAttestationLatency(check string, dur time.Duration) {
+func (d *StatsdClient) CheckAttestationLatency(check string, dur time.Duration) {
 	_ = d.client.Timing("voucher.check.attestation.latency", dur, []string{"check:" + check}, d.samplingRate)
 }
 
-func (d *StatsdMetricsClient) CheckRunError(check string, err error) {
+func (d *StatsdClient) CheckRunError(check string, err error) {
 	_ = d.client.Incr("voucher.check.run.error", []string{"check:" + check}, d.samplingRate)
 	_ = d.client.Event(createDataDogErrorEvent(check, "Voucher Check Run Error", err))
 }
 
-func (d *StatsdMetricsClient) CheckRunFailure(check string) {
+func (d *StatsdClient) CheckRunFailure(check string) {
 	_ = d.client.Incr("voucher.check.run.failure", []string{"check:" + check}, d.samplingRate)
 }
 
-func (d *StatsdMetricsClient) CheckRunSuccess(check string) {
+func (d *StatsdClient) CheckRunSuccess(check string) {
 	_ = d.client.Incr("voucher.check.run.success", []string{"check:" + check}, d.samplingRate)
 }
 
-func (d *StatsdMetricsClient) CheckAttestationStart(check string) {
+func (d *StatsdClient) CheckAttestationStart(check string) {
 	_ = d.client.Incr("voucher.check.attestation.start", []string{"check:" + check}, d.samplingRate)
 }
 
-func (d *StatsdMetricsClient) CheckAttestationSuccess(check string) {
+func (d *StatsdClient) CheckAttestationSuccess(check string) {
 	_ = d.client.Incr("voucher.check.attestation.success", []string{"check:" + check}, d.samplingRate)
 }
 
-func (d *StatsdMetricsClient) CheckAttestationError(check string, err error) {
+func (d *StatsdClient) CheckAttestationError(check string, err error) {
 	_ = d.client.Incr("voucher.check.attestation.error", []string{"check:" + check}, d.samplingRate)
 	_ = d.client.Event(createDataDogErrorEvent(check, "Voucher Check Attestation Error", err))
 }
 
 // PubSubMessageReceived tracks the number of messages received from pub/sub
-func (d *StatsdMetricsClient) PubSubMessageReceived() {
+func (d *StatsdClient) PubSubMessageReceived() {
 	_ = d.client.Incr("auto_voucher.message.received", []string{}, d.samplingRate)
 }
 
 // PubSubTotalLatency tracks the time it takes to process a pub/sub message
-func (d *StatsdMetricsClient) PubSubTotalLatency(duration time.Duration) {
+func (d *StatsdClient) PubSubTotalLatency(duration time.Duration) {
 	_ = d.client.Timing("auto_voucher.latency", duration, []string{}, d.samplingRate)
 }
 
