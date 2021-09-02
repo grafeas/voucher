6508718c091406da87ff78f74c3b198cc1a17577
Peter Wagner <peter.wagner@shopify.com>
2021-09-02 13:32:28 -0400

    datadog: introduce "backend" configuration
    We didn't like how the configuration schema required the user to know
    that statsd.addr would take precedence.
    
    This new key forces the user to be explicit, so we can provide guidance
    about further configuration in the secrets file.
    
    Co-authored-by: Dani Santos <daniele.santos@shopify.com>

--- v2/cmd/config/metrics.go
+++ v2/cmd/config/metrics.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/grafeas/voucher/v2/metrics"
@@ -9,13 +10,22 @@ import (
 
 func MetricsClient(secrets *Secrets) (metrics.Client, error) {
 	tags := viper.GetStringSlice("statsd.tags")
-	if statsdAddr := viper.GetString("statsd.addr"); statsdAddr != "" {
-		sampleRate := viper.GetFloat64("statsd.sample_rate")
-		return metrics.NewStatsdClient(statsdAddr, sampleRate, tags)
+
+	switch backend := viper.GetString("statsd.backend"); backend {
+	case "statsd", "":
+		if statsdAddr := viper.GetString("statsd.addr"); statsdAddr != "" {
+			sampleRate := viper.GetFloat64("statsd.sample_rate")
+			return metrics.NewStatsdClient(statsdAddr, sampleRate, tags)
+		}
+
+		log.Printf("No metrics client configured")
+		return &metrics.NoopClient{}, nil
+	case "datadog":
+		if secrets.Datadog.APIKey != "" && secrets.Datadog.AppKey != "" {
+			return metrics.NewDatadogClient(secrets.Datadog.APIKey, secrets.Datadog.AppKey, metrics.WithDatadogTags(tags)), nil
+		}
+		return &metrics.NoopClient{}, fmt.Errorf("missing secrets for datadog")
+	default:
+		return &metrics.NoopClient{}, fmt.Errorf("unknown statsd backend: %s", backend)
 	}
-	if secrets.Datadog.APIKey != "" && secrets.Datadog.AppKey != "" {
-		return metrics.NewDatadogClient(secrets.Datadog.APIKey, secrets.Datadog.AppKey, metrics.WithDatadogTags(tags)), nil
-	}
-	log.Printf("No metrics client configured")
-	return &metrics.NoopClient{}, nil
 }
