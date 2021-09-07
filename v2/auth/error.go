dedf166961236a445500bb2c4682add97df5015f
Peter Wagner <peter.wagner@shopify.com>
2021-09-07 10:00:46 -0400

    rename auth.AuthError auth.Error

--- v2/auth/error.go
+++ v2/auth/error.go
@@ -6,18 +6,18 @@ import (
 	"github.com/docker/distribution/reference"
 )
 
-// AuthError is the error returned when authenticating while pulling manifests connecting to protected systems
-type AuthError struct {
+// Error is the error returned when authenticating while pulling manifests connecting to protected systems
+type Error struct {
 	Reason    string
 	ImageName reference.Named
 }
 
 // Error returns a string for logging purposes
-func (e *AuthError) Error() string {
+func (e *Error) Error() string {
 	return fmt.Sprintf("auth failed: %s for %s", e.Reason, e.ImageName)
 }
 
 // NewAuthError returns an AuthError struct with the reason for erroring, as well as the Name of the image reference
 func NewAuthError(reason string, imageName reference.Named) error {
-	return &AuthError{reason, imageName}
+	return &Error{reason, imageName}
 }
