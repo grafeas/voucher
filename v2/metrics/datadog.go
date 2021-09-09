062150e8f67cab833790da57cd00054ec15ee993
Peter Wagner <peter.wagner@shopify.com>
2021-09-09 19:35:14 -0400

    batched timings, mock dogstatsd aggregation

--- v2/metrics/datadog.go
+++ v2/metrics/datadog.go
@@ -2,9 +2,12 @@ package metrics
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"math"
 	"net/url"
 	"sort"
+	"strings"
 	"sync"
 	"time"
 
@@ -69,6 +72,8 @@ type datadogStatsd struct {
 
 	mu     sync.Mutex
 	series []*datadog.Series
+	// map of timingKey() to timestamp, to values
+	durationData map[string]map[float64][]float64
 }
 
 var _ statsdClient = (*datadogStatsd)(nil)
@@ -82,10 +87,11 @@ func newDatadogStatsd(cfg *datadog.Configuration, apiKey, appKey string) *datado
 		"appKeyAuth": {Key: appKey},
 	}
 	return &datadogStatsd{
-		authCtx: context.WithValue(context.Background(), datadog.ContextAPIKeys, keys),
-		metrics: client.MetricsApi,
-		events:  client.EventsApi,
-		now:     func() float64 { return float64(time.Now().Unix()) },
+		authCtx:      context.WithValue(context.Background(), datadog.ContextAPIKeys, keys),
+		metrics:      client.MetricsApi,
+		events:       client.EventsApi,
+		now:          func() float64 { return float64(time.Now().Unix()) },
+		durationData: make(map[string]map[float64][]float64),
 	}
 }
 
@@ -124,21 +130,28 @@ func (d *datadogStatsd) Timing(metric string, dur time.Duration, tags []string, 
 	tags = append(d.tags, tags...)
 	now := d.now()
 	val := float64(dur.Milliseconds())
+	tk := timingKey(metric, tags)
 
 	d.mu.Lock()
 	defer d.mu.Unlock()
 
 	if existing := d.findSeries(metric, tags); existing != nil {
-		existing.Points = append(existing.Points, []float64{now, val})
+		d.durationData[tk][now] = append(d.durationData[tk][now], val)
 		return nil
 	}
 
 	// Not found, create
-	s := datadog.NewSeries(metric, [][]float64{{now, val}})
+	s := datadog.NewSeries(metric, nil)
 	s.SetType(durationType)
 	s.SetTags(tags)
+	s.SetInterval(1)
 	d.series = append(d.series, s)
+	d.durationData[tk] = map[float64][]float64{now: {val}}
 	return nil
+}
+
+func timingKey(metric string, tags []string) string {
+	return fmt.Sprintf("%s %s", metric, strings.Join(tags, ","))
 }
 
 func (d *datadogStatsd) Event(e *statsd.Event) error {
@@ -163,26 +176,78 @@ func (d *datadogStatsd) Event(e *statsd.Event) error {
 }
 
 func (d *datadogStatsd) submit() {
-	ctx, cancel := context.WithTimeout(d.authCtx, submitTimeout)
-	defer cancel()
-
-	d.mu.Lock()
-	defer d.mu.Unlock()
-
-	seriesCount := len(d.series)
-	if seriesCount == 0 {
+	series := d.flushSeries()
+	if len(series) == 0 {
 		return
 	}
-	series := make([]datadog.Series, 0, seriesCount)
-	for _, s := range d.series {
-		series = append(series, *s)
-	}
-	d.series = nil
+
+	ctx, cancel := context.WithTimeout(d.authCtx, submitTimeout)
+	defer cancel()
 
 	// TODO: this is sync, that is not great
 	if _, _, err := d.metrics.SubmitMetrics(ctx, *datadog.NewMetricsPayload(series)); err != nil {
 		log.Println("error submitting metrics to datadog", err)
 	}
+}
+
+func (d *datadogStatsd) flushSeries() []datadog.Series {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	series := make([]datadog.Series, 0, len(d.series))
+	for _, s := range d.series {
+		if s.GetType() != durationType {
+			series = append(series, *s)
+			continue
+		}
+
+		// Construct series from captured samples:
+		tk := timingKey(s.GetMetric(), s.GetTags())
+		var counts, averages, p95s, p99s, maxes [][]float64
+		for ts, data := range d.durationData[tk] {
+			sort.Float64s(data)
+			var sum float64
+			for _, f := range data {
+				sum += f
+			}
+			count := float64(len(data))
+			counts = append(counts, []float64{ts, count})
+			averages = append(averages, []float64{ts, sum / count})
+			p95s = append(p95s, []float64{ts, percentile(data, 0.95)})
+			p99s = append(p99s, []float64{ts, percentile(data, 0.99)})
+			maxes = append(maxes, []float64{ts, data[len(data)-1]})
+		}
+		series = append(series, cloneSeries(s, "count", counts))
+		series = append(series, cloneSeries(s, "avg", averages))
+		series = append(series, cloneSeries(s, "95percentile", p95s))
+		series = append(series, cloneSeries(s, "99percentile", p99s))
+		series = append(series, cloneSeries(s, "max", maxes))
+	}
+	d.series = nil
+	d.durationData = make(map[string]map[float64][]float64)
+	return series
+}
+
+func cloneSeries(s *datadog.Series, suffix string, points [][]float64) datadog.Series {
+	return datadog.Series{
+		Host:           s.Host,
+		Interval:       s.Interval,
+		Metric:         fmt.Sprintf("%s.%s", s.GetMetric(), suffix),
+		Points:         points,
+		Tags:           s.Tags,
+		Type:           s.Type,
+		UnparsedObject: s.UnparsedObject,
+	}
+}
+
+func percentile(data []float64, p float64) float64 {
+	pos := float64(len(data)) * p
+	if math.Round(pos) == pos {
+		// Return exact value at percentile
+		return data[int(pos)]
+	}
+
+	return (data[int(pos-1)] + data[int(pos)]) / 2
 }
 
 func (d *datadogStatsd) findSeries(metric string, tags []string) *datadog.Series {
