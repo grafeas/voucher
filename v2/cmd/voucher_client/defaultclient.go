7d2aedea94be020bcc459dd4e4417c64eb98fe28
Brian Chen <brian.chen@shopify.com>
2021-12-16 17:50:23 -0500

    More testing with generating tokens

--- v2/cmd/voucher_client/defaultclient.go
+++ v2/cmd/voucher_client/defaultclient.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -39,25 +41,71 @@ func (s *idTokenSource) Token() (*oauth2.Token, error) {
 	}, nil
 }
 
+func (s *idTokenSource) HasIdToken() bool {
+	// Check for idToken
+	if token, err := s.Token(); err == nil {
+		_, ok := token.Extra(idTokenKey).(string)
+		return ok
+	}
+	return false
+}
+
 func getDefaultTokenSourceClient(ctx context.Context) (*http.Client, error) {
 	src, err := google.DefaultTokenSource(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("error creating token source: %w", err)
 	}
 
-	ts := oauth2.ReuseTokenSource(nil, &idTokenSource{TokenSource: src})
-
-	// Make Client to generate token
-	googleOauth2Service, err := goauth2.NewService(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("error connecting to google oauth2: %w", err)
+	tokenSource := &idTokenSource{TokenSource: src}
+	if !tokenSource.HasIdToken() {
+		// Make Client to generate token
+		// generate idtoken
+		generateIdToken(ctx)
 	}
-	tokenInfo, err := googleOauth2Service.Tokeninfo().Do()
-	fmt.Printf("stuff: %v\n", tokenInfo.Email)
 
+	ts := oauth2.ReuseTokenSource(nil, tokenSource)
 	transport, err := htransport.NewTransport(ctx, http.DefaultTransport, option.WithTokenSource(ts))
 	if err != nil {
 		return nil, fmt.Errorf("error creating client: %w", err)
 	}
 	return &http.Client{Transport: transport}, nil
+}
+
+func generateIdToken(ctx context.Context) {
+	googleOauth2Service, err := goauth2.NewService(ctx)
+	if err != nil {
+		fmt.Errorf("connecting to google oauth2 service to generate idToken: %w", err)
+	}
+	tokenInfo, err := googleOauth2Service.Tokeninfo().Do()
+	if err != nil {
+		fmt.Errorf("get token info: %w", err)
+	}
+
+	stuff := map[string]string{
+		"audience": defaultConfig.Server,
+	}
+
+	body, err := json.Marshal(stuff)
+	if err != nil {
+		fmt.Errorf("get id token into: %w", err)
+	}
+
+	fmt.Printf("%v\n", body)
+	client, err := google.DefaultClient(ctx, "https://www.googleapis.com/auth/cloud-platform")
+	req, err := http.NewRequest(
+		http.MethodPost,
+		fmt.Sprintf("https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/%s:generateIdToken", tokenInfo.Email),
+		bytes.NewBuffer(body),
+	)
+	if err != nil {
+		fmt.Errorf("get id token into: %w", err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Errorf("get id token into: %w", err)
+	}
+	defer resp.Body.Close()
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
 }
