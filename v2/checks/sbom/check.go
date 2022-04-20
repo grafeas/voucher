2c2794610057ea2e9f16e885810a834a72d34a4f
Daniele Santos <daniele.santos@shopify.com>
2022-04-20 09:42:50 -0400

    some more ideas

--- v2/checks/sbom/check.go
+++ v2/checks/sbom/check.go
@@ -1,29 +1,55 @@
 package sbom
 
 import (
-	"github.com/spdx/tools-golang"
+	"context"
+	"errors"
+
+	"github.com/grafeas/voucher/v2"
 )
 
 // There are two supported versions in
 // https://github.com/spdx/tools-golang/tree/main/spdx
 // i.e. 2.1 and 2.2; we'll need to support both for spdx
 
-type SBOMResponse struct {
-	SPDXVersion string `json:"spdxversion"`
+// check is a check that verifies if there's an sbom attached with
+// the container image
+type check struct {
+	auth voucher.Auth
 }
 
-type SBOM interface {
-	GetSBOM() string
-	GetVersion() string
+// ErrNoSBOMFound  is returned when an image does not have
+// any sboms attached to it
+var ErrNoSBOMFound = errors.New("image has no sbom attached")
+
+// SetAuth sets the authentication system that this check will use
+// for its run.
+func (c *check) SetAuth(auth voucher.Auth) {
+	c.auth = auth
 }
 
-func (s *SBOMResponse) GetVersion() string {
-	return s.SPDXVersion
+// hasSBOM returns true if the passed image has an SBOM attached
+func (c *check) hasSBOM(i voucher.ImageData) bool {
+	// TODO: call GCR to check for the presence of an SBOM
+
+	// digest := i.Digest()
+
+	// TODO: query GCR to see if there's an OCI manifest whose tag
+	// contains the digest
+
+	return false
 }
 
-func (s *SBOMResponse) GetSBOM() string {
+// check checks if an image was built by a trusted source
+func (c *check) Check(ctx context.Context, i voucher.ImageData) (bool, error) {
+	if !c.hasSBOM(i) {
+		return false, ErrNoSBOMFound
+	}
 
+	return true, nil
 }
 
-// CreationInfo2_1
-// https://github.com/spdx/tools-golang/blob/8e09d22f514a2eeee9a3044fb47d0cb6dbc74a6a/spdx/creation_info.go
+func init() {
+	voucher.RegisterCheckFactory("sbom", func() voucher.Check {
+		return new(check)
+	})
+}
