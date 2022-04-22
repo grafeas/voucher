ece26db705462b15dd4a2cf5cf34d5e453663ea9
Chris Shino <chris.shino@shopify.com>
2022-04-22 11:37:30 -0400

    added get repo method and client creation

--- v2/go.mod
+++ v2/go.mod
@@ -15,34 +15,33 @@ require (
 	github.com/antihax/optional v1.0.0
 	github.com/bradleyfalzon/ghinstallation v1.1.1
 	github.com/coreos/clair v2.0.5+incompatible
-	github.com/docker/distribution v2.6.0-rc.1.0.20180913220339-b089e9168825+incompatible
+	github.com/docker/distribution v2.7.1+incompatible
 	github.com/docker/docker v20.10.14+incompatible
 	github.com/docker/libtrust v0.0.0-20160708172513-aabc10ec26b7
 	github.com/dustin/gojson v0.0.0-20160307161227-2e71ec9dd5ad // indirect
 	github.com/fernet/fernet-go v0.0.0-20211208181803-9f70042a33ee // indirect
 	github.com/golang/mock v1.6.0
+	github.com/google/go-containerregistry v0.8.0
 	github.com/googleapis/gax-go/v2 v2.1.1
-	github.com/gorilla/context v1.1.1 // indirect
-	github.com/gorilla/mux v1.6.2
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/gorilla/mux v1.8.0
 	github.com/mennanov/fieldmask-utils v0.0.0-20190703161732-eca3212cf9f3
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/opencontainers/go-digest v1.0.0-rc1
+	github.com/opencontainers/go-digest v1.0.0
 	github.com/opencontainers/image-spec v1.0.2 // indirect
 	github.com/pborman/uuid v0.0.0-20180906182336-adf5a7427709 // indirect
 	github.com/shurcooL/githubv4 v0.0.0-20190718010115-4ba037080260
 	github.com/shurcooL/graphql v0.0.0-20181231061246-d48a9a75455f // indirect
-	github.com/sirupsen/logrus v1.4.2
+	github.com/sirupsen/logrus v1.8.1
 	github.com/smartystreets/goconvey v0.0.0-20190731233626-505e41936337 // indirect
-	github.com/spf13/cobra v0.0.3
-	github.com/spf13/viper v1.4.0
+	github.com/spf13/cobra v1.3.0
+	github.com/spf13/viper v1.10.0
 	github.com/stretchr/testify v1.7.1
 	go.mozilla.org/sops/v3 v3.0.0-00010101000000-000000000000
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
 	google.golang.org/api v0.63.0
 	google.golang.org/genproto v0.0.0-20211208223120-3a66f561d7aa
-	google.golang.org/grpc v1.40.1
+	google.golang.org/grpc v1.43.0
 	gotest.tools/v3 v3.1.0 // indirect
 )
 
--- v2/sbomgcr/client.go
+++ v2/sbomgcr/client.go
@@ -2,16 +2,20 @@ package sbomgcr
 
 import (
 	"context"
+	"fmt"
 	"strings"
 
 	"github.com/CycloneDX/cyclonedx-go"
 	"github.com/docker/distribution/reference"
+	"github.com/google/go-containerregistry/pkg/authn"
+	"github.com/google/go-containerregistry/pkg/name"
+	gcr "github.com/google/go-containerregistry/pkg/v1/google"
 	voucher "github.com/grafeas/voucher/v2"
-	"github.com/grafeas/voucher/v2/signer"
 )
 
 // Client connects to GCR
 type Client struct {
+	authenticator authn.Authenticator
 }
 
 // GetVulnerabilities returns the detected vulnerabilities for the Image described by voucher.ImageData.
@@ -20,21 +24,41 @@ func (c *Client) GetVulnerabilities(ctx context.Context, ref reference.Canonical
 }
 
 // GetSBOM gets the SBOM for the passed image.
-func (g *Client) GetSBOM(ctx context.Context, ref reference.Canonical) (cyclonedx.BOM, error) {
+func (c *Client) GetSBOM(ctx context.Context, ref reference.Canonical) (cyclonedx.BOM, error) {
 	digest := ref.Digest().String()
 	tag := strings.Replace(digest, "@", "-", 1)
-	// get repo (similar to gcloud describe) or parse as ReferenceToProjectName
-	// lets authenticate this thing
-	// list tags https://pkg.go.dev/github.com/google/go-containerregistry@v0.8.0/pkg/v1/google#List
-	// match tag to sbom
-	// get sbom
+
+	// we can call the GetRepoTags method to get back all the gcr.Tags on a specific repo
+	// then match the tag we make from the digest against it
+	// then get the sbom
 
 	return cyclonedx.BOM{}, nil
 }
 
+// GetRepoTags gets the gcr tags for the passed image.
+func (c *Client) GetRepoTags(ctx context.Context, repoName string) (*gcr.Tags, error) {
+	repository, err := name.NewRepository(repoName)
+	if err != nil {
+		fmt.Errorf("error returning authenticator %w", err)
+	}
+
+	tags, err := gcr.List(repository, gcr.WithAuth(c.authenticator), gcr.WithContext(ctx))
+	if err != nil {
+		return nil, fmt.Errorf("error occured when trying to retrieve tags from this repo %w", err)
+	}
+
+	return tags, err
+}
+
 // NewClient creates a new
-func NewClient(ctx context.Context, binauthProject string, keyring signer.AttestationSigner) (*Client, error) {
-	client := &Client{}
+func NewClient() (*Client, error) {
+	// This authenticator is the only one that seems to use ADC?? lets try it
+	auth, err := gcr.NewEnvAuthenticator()
+	if err != nil {
+		fmt.Errorf("error returning authenticator %w", err)
+	}
+
+	client := &Client{authenticator: auth}
 
 	return client, nil
 }
